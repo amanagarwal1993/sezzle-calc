d06a79af2db293f482462502c62f45cd7e1bd927
Aman Agarwal <aman@starskyrobotics.com>
2020-06-15 12:18:32 -0700

    Update database

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask_socketio import SocketIO, emit
 from flask_session import Session
 from operator import pow, truediv, mul, add, sub
 from simpleeval import simple_eval
+from app.models import Result
 
 session = Session(flaskapp)
 socketio = SocketIO(flaskapp, manage_session=False)
@@ -15,7 +16,12 @@ def index():
 @socketio.on('calculate', namespace='/test')
 def calculate_expression(query):
     try:
-        answer = simple_eval(query)    
+        answer = simple_eval(query)
+        
+        result = Result(expr=query, result=answer)
+        db.session.add(result)
+        db.session.commit()
+        
         emit('result', {'query': query, 'answer': answer}, broadcast=False)
         emit('calculation', {'query': query, 'answer': answer}, broadcast=True)
     except:
@@ -25,7 +31,9 @@ def calculate_expression(query):
 @socketio.on('connect', namespace='/test')
 def test_connect():
     emit('connection', {'data': 'Connected'})
+    prev_results = Result.query.all()
     print ('Client connected')
+    print (prev_results)
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
